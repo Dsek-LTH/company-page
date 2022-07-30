abdc0cc6b49a15cd6225ad9889bdeeb3ecc2e900
Ludvig Svedberg <ludvig.svedberg@mac.com>
2022-07-30 19:18:37 +0200

    Add placeholder pages

---
+++ pages/order.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import MainLayout from "../components/layout/MainLayout";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Företag | D-Sektionen inom TLTH</title>
+        <meta
+          name="description"
+          content="Lär dig mer om hur du kan på bästa sättet synas inför våra studenter!"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      Beställ
+    </>
+  );
+};
+
+export default Home;
